67958aac1ee3f81c68ddbc4b709a6103acad4225
Tatsuro Miyazaki <tatsumiya05@gmail.com>
2021-09-09 01:03:45 +0900

    Add database template

---
+++ unta_food/databse/deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+readonly SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+source ${SCRIPT_DIR}/../config.sh
+
+readonly TEMPLATE_FILE="${SCRIPT_DIR}/template.yml"
+
+aws cloudformation deploy \
+  --no-fail-on-empty-changeset \
+  --region ${REGION} \
+  --stack-name ${DATABASE_STACK_NAME} \
+  --template-file ${TEMPLATE_FILE} \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    UTNAFoodTableName=${UTNA_FOOD_TABLE_NAME} \
+    UTNAFoodRegistrationStatusTableName=${REGISTRATION_STATUS_TABLE_NAME}
+
+aws cloudformation describe-stacks \
+  --region ${REGION} \
+  --stack-name ${DATABASE_STACK_NAME} \
+  --output table
