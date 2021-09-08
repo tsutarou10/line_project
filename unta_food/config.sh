21fe3cdff651b47dcc86e139bb2946fd7b3c0789
Tatsuro Miyazaki <tatsumiya05@gmail.com>
2021-09-09 01:04:02 +0900

    Update config file

--- unta_food/config.sh
+++ unta_food/config.sh
@@ -3,16 +3,21 @@
 set -e
 set -u
 
+# ==== common ====
 readonly SERVICE_NAME="utna-food"
-
 readonly REGION="ap-northeast-1"
 
+# ==== setup ====
 readonly SETUP_STACK_NAME="${SERVICE_NAME}-setup"
 readonly ARTIFACT_BUCKET_NAME="${SERVICE_NAME}-artifact-bucket"
 readonly ARTIFACT_BUCKET_PREFIX="cloudformation/${SERVICE_NAME}"
 
+# ==== database ====
+readonly DATABASE_STACK_NAME="${SERVICE_NAME}-database"
+readonly UTNA_FOOD_TABLE_NAME="UTNAFood"
+readonly REGISTRATION_STATUS_TABLE_NAME="UTNAFoodRegistrationStatus"
+
+# ==== service ====
 readonly SERVICE_STACK_NAME="${SERVICE_NAME}-stack"
 readonly FUNCTION_NAME="${SERVICE_NAME}-Function"
-readonly TABLE_NAME="UTNAFood"
-
 readonly API_NAME="${SERVICE_NAME}-API"
